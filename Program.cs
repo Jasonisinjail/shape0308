c737623943542b07ef6e1292ee7bc4fdf9785d64
jamesleu923 <jamesleu923@E612-PC10>
2025-03-06 09:47:54 +0800

    20250308完成三角形,圓形面積比較, 使用父類別的compare to..
    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using shape;
+using System.Net;
+
+//Shape myshape = new Shape();
+
+Shape mycircle1 = new Circle(2.14);
+//Shape mycircle2 = new Circle(23.5);
+Shape myTrianble01 = new Triangle(9.56,10.8);
+//Shape myTrianble02 = new Triangle(36.8, 10);
+Shape myTrapezoid01 = new TrapeZoid(19.55, 3.145, 10.9);
+//Shape myTrapezoid02 = new TrapeZoid(10, 20, 30);
+
+
+Console.WriteLine("圓形1:" + mycircle1.area.ToString());
+Console.WriteLine("三角形01:" + myTrianble01.area.ToString());
+Console.WriteLine("梯形01:" + myTrapezoid01.area.ToString());
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+string msg = "";
+if (mycircle1.CompareTo(myTrianble01) == 0)
+{
+    if (myTrapezoid01.CompareTo(mycircle1) == 0)
+        msg = "一樣大";
+}
+if (mycircle1.CompareTo(myTrianble01) > 0)
+{
+    if (mycircle1.CompareTo(myTrapezoid01) > 0) msg = "圓比較大";
+    else msg = "梯形比較大";
+}
+
+if (mycircle1.CompareTo(myTrianble01) < 0)
+{
+    if(myTrianble01.CompareTo(myTrapezoid01)>0)msg = "三角形比較大"; 
+    else msg = "梯形比較大";
+}
+Console.WriteLine(msg);
---
+++ TrapeZoid.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape
+{
+    internal class TrapeZoid : Shape
+    {
+        private readonly double value;
+        private double _Upperbottom;
+        private double _LowerBottom;
+        private double _high;
+
+        public double Upperbottom
+        {
+            get { return _Upperbottom; }
+            set { _Upperbottom = value; }
+        }
+        public double High
+        {
+            get { return _high; }
+            set{_high = value; }
+        }
+        public double Lowerbottom
+        {
+            get { return _LowerBottom; }
+            set { _LowerBottom = value; }
+        }
+        public TrapeZoid() { }
+        public TrapeZoid(double Upperbottom, double Lowerbottom, double High)
+        {
+            this.Upperbottom = Upperbottom;
+            this.Lowerbottom = Lowerbottom;
+            this.High = High;
+            this.CountArea();
+        }
+
+        public override void CountArea()
+        {
+            double bottom = this.Upperbottom+this.Lowerbottom;
+            this.area = (bottom * this.High) / 2;
+        }
+
+    }
+}
